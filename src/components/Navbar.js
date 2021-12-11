dab0fb2e3ae30166af4e695694e7af7559cdb0ff
Oooooiii <hearong.200@gmail.com>
2021-12-11 20:36:52 +0700

    fn navar

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import './NavbarStyles.css'
+
+const Navbar = () => {
+    const [click, setClick] = useState(false)
+    const handleClick = () => { setClick(!click) }
+
+    return (
+        <header>
+            <nav className='navbar'>
+                <div className='logo'>
+                    <h1>Logo</h1>
+                </div>
+
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link className='nav-link' to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className='nav-link' to="/">Pricing</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className='nav-link' to="/">FQA</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className='nav-link' to="/">Contact</Link>
+                    </li>
+                </ul>
+
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes size={20} style={{ color: "#fff" }} />) : (<FaBars size={20} style={{ color: "#fff" }} />)}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
