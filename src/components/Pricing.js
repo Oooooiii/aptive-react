2f13d6320e32718466710a36f6177fe5abb5d3d6
Oooooiii <hearong.200@gmail.com>
2021-12-12 00:04:03 +0700

    fn pricing

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import './PricingStyles.css'
 
 const Pricing = () => {
     return (
-        <div>
+        <div className="pricing">
+            <div className="container">
+                <div className="top-content">
+                    <div>
+                        <h2>Everything you need for <span>$99 a month</span></h2>
+                        <p>Includes every feature we offer plus unlimited projects and unlimited users.</p>
+                    </div>
+                    <div className="btn-div">
+                        <button>Get started today</button>
+                    </div>
+                </div>
 
+                <div className="bottom-content">
+                    <div className="card">
+                        <p><span>Everything you need</span></p>
+                        <h3>All-in-one platform</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    </div>
+                    <div className="card">
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
